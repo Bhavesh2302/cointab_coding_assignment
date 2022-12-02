a9a560049b1f9f1836b3e527b4a6737a2eb7ba05
Bhavesh2302 <jainbhavesh888@gmail.com>
2022-12-02 20:07:02 +0530

    established the connection with the port 7500

--- server/index.js
+++ server/index.js
@@ -1,2 +1,25 @@
 
 const express  = require("express")
+const { connection } = require("./configs/db")
+require("dotenv").config()
+const cors = require("cors")
+const app = express()
+
+const PORT = process.env.PORT || 7500
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+
+    res.send("hello there")
+})
+
+app.listen(PORT, async()=>{
+    try {
+        await connection
+        console.log("running on port: http://localhost:7500")
+    } catch (error) {
+        console.log(error)
+    }
+})
